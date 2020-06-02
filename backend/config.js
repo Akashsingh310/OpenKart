dd6d4992a04b270522ff2710386dc710fc9be2f9
Akash Singh <akash22675@gmail.com>
2020-06-02 13:25:38 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+
+
+export default {
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/OpenKart',
+  JWT_SECRET: process.env.JWT_SECRET || '5ed4cd87eea8964858c536e3',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AYGBrms_pgvAjAS7PX-6pPG_F26ShqpKlxSZxnNET-Yy8acuGLzjEHEsigZP0uTqk9MtlIGso4tKPIuG'
+}
